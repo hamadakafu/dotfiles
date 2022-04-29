e040e036aa4aa9d1c3357c960af24c51d00c5e9c
hamadakafu <kafu.h1998@gmail.com>
2022-04-29 13:47:39 +0900

    arch

--- arch-automation1.sh
+++ arch-automation1.sh
@@ -2,9 +2,18 @@ sudo pacman -Syy
 sudo pacman -Syyu
 sudo pacman -Syu \
     git \
+    base-devel \
+    glibc \
+    icu \
+    xorg-xwayland \
+    rofi \
     sway \
     swayidle \
     swaylock \
     zsh \
-    tmux \
-    neovim
+    neovim \
+    just \
+    subversion \
+    microsoft-edge-stable-bin
+
+yay -S $(yay -Si neovim | grep '^Depends On' | sed 's/Depends On.*://')
