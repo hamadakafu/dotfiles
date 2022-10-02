3ccb00f0f4380e8ee46233989fa5e664a4625974
hamadakafu <kafu.h1998@gmail.com>
2022-10-02 20:30:15 +0900

    wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -15,6 +15,8 @@ return {
         timeout_milliseconds = 2000,
       }
     } },
+    { key = "/", mods = "CTRL", action = wezterm.action.ActivateCopyMode },
+
     { key = "v", mods = "CMD", action = wezterm.action.Paste },
     { key = "c", mods = "CMD", action = wezterm.action.Copy },
     -- { key = "e", mods = "CTRL", action = wezterm.action.SendKey { key = "End" } },
@@ -44,7 +46,40 @@ return {
       { key = "l", mods = "CTRL", action = wezterm.action { ActivateTabRelative = 1 }, },
       { key = "h", mods = "CTRL", action = wezterm.action { ActivateTabRelative = -1 },
       },
-
+    },
+    copy_mode = {
+      { key = "Escape", action = wezterm.action { CopyMode = "Close" } },
+      { key = "h", action = wezterm.action { CopyMode = "MoveLeft" } },
+      { key = "l", action = wezterm.action { CopyMode = "MoveRight" } },
+      { key = "k", action = wezterm.action { CopyMode = "MoveUp" } },
+      { key = "j", action = wezterm.action { CopyMode = "MoveDown" } },
+      { key = "b", action = wezterm.action { CopyMode = "MoveBackwardWord" } },
+      { key = "w", action = wezterm.action { CopyMode = "MoveForwardWord" } },
+      { key = "a", mods = "CTRL", action = wezterm.action { CopyMode = "MoveToStartOfLine" } },
+      { key = "e", mods = "CTRL", action = wezterm.action { CopyMode = "MoveToEndOfLineContent" } },
+      {
+        key = 'v',
+        mods = 'NONE',
+        action = wezterm.action.CopyMode { SetSelectionMode = 'Cell' },
+      },
+      {
+        key = 'v',
+        mods = 'SHIFT',
+        action = wezterm.action.CopyMode { SetSelectionMode = 'Cell' },
+      },
+      {
+        key = 'v',
+        mods = 'CTRL',
+        action = wezterm.action.CopyMode { SetSelectionMode = 'Block' },
+      },
+      {
+        key = 'y',
+        mods = 'NONE',
+        action = wezterm.action.Multiple {
+          { CopyTo = 'ClipboardAndPrimarySelection' },
+          { CopyMode = 'Close' },
+        },
+      },
     },
   },
 }
