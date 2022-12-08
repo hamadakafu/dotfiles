f0619e69cf0c852fc39df110491ba5782922d3f0
hamadakafu <kafu.h1998@gmail.com>
2022-12-08 17:26:37 +0900

    afff

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,27 +4,28 @@ hs.hotkey.bind({ "cmd" }, "space", function()
   local wezterm = hs.application.get('WezTerm')
   if wezterm == nil then
     hs.application.launchOrFocus("/Applications/WezTerm.app")
+    return
+  end
+
+  local space_id = spaces.focusedSpace()
+  -- hs.alert.show(space_id)
+  local wezwin = wezterm:allWindows()
+
+  if wezwin[1] and wezwin[1]:isVisible() and wezterm:isFrontmost() then
+    -- 現在のspaceにあって見える状態でfrontにいる場合
+    wezterm:hide()
+  elseif wezwin[1] and wezwin[1]:isVisible() then
+    -- 現在のspaceにあって見えるnot frontの場合
+    wezterm:setFrontmost()
+  elseif wezwin[1] then
+    -- 現在のspaceにあって見えない状態
+    spaces.moveWindowToSpace(wezwin[1], space_id)
+    wezterm:setFrontmost() -- hideも解消
   else
-    local space_id = spaces.focusedSpace()
-    -- hs.alert.show(space_id)
-
-    local wezwin = wezterm:allWindows()
-    if wezwin[1] == nil then
-      local wf_wezterm = hs.window.filter.new{'WezTerm'}:getWindows()
-      spaces.moveWindowToSpace(wf_wezterm[1], space_id)
-      wezterm:setFrontmost()
-      return
-    end
-
-
-    if wezwin[1] and wezwin[1]:isVisible() and wezterm:isFrontmost() then
-      wezterm:hide()
-    elseif wezwin[1] and wezwin[1]:isVisible() and (not wezterm:isFrontmost()) then
-      wezterm:setFrontmost()
-    elseif wezwin[1] and not wezwin[1]:isVisible() then
-      spaces.moveWindowToSpace(wezwin[1], space_id)
-      wezterm:unhide()
-      wezterm:setFrontmost()
-    end
+    -- 現在のspaceにない状態
+    local wf_wezterm = hs.window.filter.new { 'WezTerm' }:getWindows()
+    spaces.moveWindowToSpace(wf_wezterm[1], space_id)
+    wezterm:setFrontmost() -- hideも解消
+    return
   end
 end)
